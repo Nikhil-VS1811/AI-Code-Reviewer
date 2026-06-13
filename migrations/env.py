d06dee6b5cd8d8736345918aaec085ed2d651631
Nikhil VS <nikhilvenkatesh1811@gmail.com>
2026-06-13 16:19:16 +0530

    Fix Alembic for psycopg

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,7 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine, pool
 
 from app.core.config import settings
 from app.db.base import Base
@@ -32,10 +32,8 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    configuration = config.get_section(config.config_ini_section, {})
-    connectable = engine_from_config(
-        configuration,
-        prefix="sqlalchemy.",
+    connectable = create_engine(
+        settings.DATABASE_URL,
         poolclass=pool.NullPool,
     )
 
